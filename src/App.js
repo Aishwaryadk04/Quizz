8cf8427e8a80989f750c9b4476daf15351563a33
Aishwaryadk04 <aishwaryadkofficial@gmail.com>
2023-11-25 12:16:53 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { quizData } from './Components/quizzData';
+
+const backgroundImages =  [
+  'https://i.natgeofe.com/k/42e832f5-fd48-43ff-b338-091bdf4048ca/india-tajmahal_16x9.jpg?w=1200',
+  'https://static.toiimg.com/thumb/msid-80421029,width-1280,resizemode-4/80421029.jpg',
+  'https://www.g20.org/content/dam/gtwenty/gtwenty_new/explore_india/Culture%20Heritage%20of%20India_1.jpg',
+  'https://media.istockphoto.com/id/1146517111/photo/taj-mahal-mausoleum-in-agra.jpg?s=612x612&w=0&k=20&c=vcIjhwUrNyjoKbGbAQ5sOcEzDUgOfCsm9ySmJ8gNeRk=',
+  'https://images.contentstack.io/v3/assets/bltacc1a01c4d280f24/blt5f4264cfc4ab9e8c/61847e47912a3c799bc42c4c/Taj_Mahal_New_Delhi_India.jpg?auto=webp&format=pjpg&quality=80&width=900&height=500&fit=crop',
+  'https://www.bridgeindia.org.uk/wp-content/uploads/2022/01/home-header-image-1600x900.jpg',
+  'https://cdn.britannica.com/12/1612-050-8A4D277F/Settlement-Kullu-Valley-India-Himachal-Pradesh.jpg',
+  'https://images.axios.com/wLi0s3Pcl6LVFNheenhlXWG_NiU=/0x638:6123x4082/1920x1080/2023/01/09/1673275741989.jpg',
+  'https://www.japantimes.co.jp/uploads/imported_images/uploads/2023/03/np_file_217171.jpeg',
+  'https://www.travelandleisure.com/thmb/iAIrOVW7yWrDG8pZBpKMOvEGuNQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/new-delhi-india-NEWDELHITG0721-60d592e1603349298a0206d67d08582b.jpg'
+
+ 
+ ];
 
 function App() {
+  const [ currentQ, setCurrentQ] = useState(0)
+  const [showScore, setShowScore] = useState(false)
+  const [score,setScore] = useState(0)
+  const [bg,setBg] = useState('https://images2.alphacoders.com/132/1329016.pnghttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuq4hvcAiZ_ECPBo_A4cIYkHEabMn077wm4g&usqp=CAU')
+ const getRandomBg = ()=>{
+  const randomBg = backgroundImages[Math.floor(Math.random()*backgroundImages.length)]
+  setBg(randomBg)
+ }
+ const handleAnswerButton = (isCorrect)=>{
+  if(isCorrect === true){
+    setScore(score + 1)
+    
+  }
+  const nextQ = currentQ +1;
+  if(nextQ < quizData.length){
+    setCurrentQ(nextQ)
+  } else{
+    setShowScore(true)
+  }
+}
+
+ useEffect(()=>{
+  getRandomBg()
+ },[handleAnswerButton])
+
+ const handleReset =()=>{
+  setCurrentQ(0)
+  setShowScore(false)
+  setScore(0)
+ }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body' style={{background:`url(${bg})`,transition:'background-image 0.75s ease-out',height:'100vh'}}>
+    <div className='container'>
+      <h1 style={{color:'darkgrey', fontSize:'40px'}}>My India </h1>
+         {showScore?
+          (
+          <div className='score-card'>
+            Your Score : <br /> <span style={{fontSize:'90px'}}>{score}/10</span>
+            <div className="replay-btn">
+              <button onClick={handleReset}>Try Again</button>
+            </div>
+          </div>
+           ):
+           (
+          <div className='question-card'>
+            <h3 className='qNum'>{currentQ+1}/{quizData.length}</h3>
+            <h3 className='qText'>{quizData[currentQ].questionText}</h3>
+            {quizData[currentQ].answerOption.map((answerOption)=>(
+               <div className='btn'>
+              <button onClick={()=>handleAnswerButton(answerOption.isCorrect)}>{answerOption.answerText}</button>
+              </div>
+            ))}
+          </div>
+           )
+            }
+    </div>
     </div>
   );
 }
---
+++ src/Components/quizzData.js
@@ -0,0 +1,92 @@
+export const quizData = [
+    {
+        questionText:'Which of the following is the capital of Arunachal Pradesh?',
+        answerOption: [
+            {answerText:'Dispur',isCorrect: false},
+            {answerText:'Itanagar',isCorrect: true},
+            {answerText:'Panaji',isCorrect: false},
+            {answerText:'Imphal',isCorrect: false}
+         ]
+    },
+    {
+        questionText:'Which of the following states is not located in the North?',
+        answerOption: [
+            {answerText:'Haryana',isCorrect: false},
+            {answerText:' Himachal Pradesh',isCorrect: false},
+            {answerText:' Jammu and Kashmir',isCorrect: false},
+            {answerText:'Jharkhand',isCorrect: true}
+         ]
+    },
+    {
+        questionText:'Which is the largest coffee-producing state of India?',
+        answerOption: [
+            {answerText:'Karnataka',isCorrect: true},
+            {answerText:'Tamil Nadu',isCorrect: false},
+            {answerText:'Kerala',isCorrect: false},
+            {answerText:'Arunachal Pradesh',isCorrect: false}
+         ]
+    },
+    {
+        questionText:'Which state has the largest area?',
+        answerOption: [
+            {answerText:'Uttar Pradesh',isCorrect: false},
+            {answerText:'Rajasthan',isCorrect: true},
+            {answerText:'Maharashtra',isCorrect: false},
+            {answerText:'Madhya Pradesh',isCorrect: false}
+         ]
+    },
+    {
+        questionText:' Which state has the largest population?',
+        answerOption: [
+            {answerText:'Maharashtra',isCorrect: false},
+            {answerText:'Bihar',isCorrect: false},
+            {answerText:'Uttar Pradesh',isCorrect: true},
+            {answerText:'Rajasthan',isCorrect: false}
+         ]
+    },
+    {
+        questionText:' In what state is Elephant Falls located?',
+        answerOption: [
+            {answerText:'Meghalaya',isCorrect: true},
+            {answerText:'Orissa',isCorrect: false},
+            {answerText:'Manipur',isCorrect: false},
+            {answerText:'Mizoram',isCorrect: false}
+         ]
+    },
+    {
+        questionText:'What is the staple drink of Goa?',
+        answerOption: [
+            {answerText:'Toddy',isCorrect: false},
+            {answerText:'Thandai',isCorrect: false},
+            {answerText:'Feni',isCorrect: true},
+            {answerText:'Sattu Sharbath',isCorrect: false}
+         ]
+    },
+    {
+        questionText:'Yakshagana is the folk dance of which state?',
+        answerOption: [
+            {answerText:'Karnataka',isCorrect: true},
+            {answerText:'Kerala',isCorrect: false},
+            {answerText:'Telangana',isCorrect: false},
+            {answerText:'Tamil Nadu',isCorrect: false}
+         ]
+    },
+    {
+        questionText:'What is the Capital of Mizoram?',
+        answerOption: [
+            {answerText:'Jaipur',isCorrect: false},
+            {answerText:'Aizawal',isCorrect: true},
+            {answerText:'Gangtok',isCorrect: false},
+            {answerText:'Khawzawl',isCorrect: false}
+         ]
+    },
+    {
+        questionText:'Which city is known as the “summer capital” of Jammu and Kashmir?',
+        answerOption: [
+            {answerText:'Jammu',isCorrect: false},
+            {answerText:'Anantnag',isCorrect: false},
+            {answerText:'Shimla',isCorrect: false},
+            {answerText:'Srinagar',isCorrect: true}
+         ]
+    }
+]
